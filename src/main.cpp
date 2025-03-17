90ae6a5c4ee31f03d2bbba15261b904f4b23b4bb
MaxJakaria <maxjakaria825@gmail.com>
2025-03-17 20:16:51 +0600

    Complete 3rd Assignment (Control load with IR Remote)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <IRremote.hpp>
 
-const int IR_RECIVER_PIN = 15;
+const int IR_RECEIVER_PIN = 15;
+const int pins[] = {27, 14, 12, 13};
+bool checkStatus[] = {false, false, false, false};
 
 void setup()
 {
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(pins[i], OUTPUT);
+  }
   Serial.begin(115200);
-  IrReceiver.begin(IR_RECIVER_PIN, ENABLE_LED_FEEDBACK);
+  IrReceiver.begin(IR_RECEIVER_PIN, ENABLE_LED_FEEDBACK);
 }
 
 void loop()
@@ -14,6 +20,40 @@ void loop()
   {
     String ir = String(IrReceiver.decodedIRData.command, HEX);
     Serial.println(ir);
+
+    if (ir == "40")
+    {
+      for (int i = 3; i >= 0; i--)
+      {
+        if (checkStatus[i])
+        {
+          digitalWrite(pins[i], LOW);
+          checkStatus[i] = false;
+          break;
+        }
+      }
+    }
+
+    if (ir == "c")
+    {
+      digitalWrite(pins[0], HIGH);
+      checkStatus[0] = true;
+    }
+    if (ir == "18")
+    {
+      digitalWrite(pins[1], HIGH);
+      checkStatus[1] = true;
+    }
+    if (ir == "5e")
+    {
+      digitalWrite(pins[2], HIGH);
+      checkStatus[2] = true;
+    }
+    if (ir == "8")
+    {
+      digitalWrite(pins[3], HIGH);
+      checkStatus[3] = true;
+    }
 
     IrReceiver.resume();
   }
